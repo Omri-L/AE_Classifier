24d46249c4196b3274836492f6a8ae9d4216927b
Paz87 <pazz87@gmail.com>
2020-07-21 20:50:12 +0300

    Added missing file

---
+++ Config.py
@@ -0,0 +1,30 @@
+import numpy as np
+import time
+from collections import OrderedDict
+import torch
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from sklearn.metrics import roc_auc_score
+from DatasetGenerator import DatasetGenerator
+import PIL
+import matplotlib.pyplot as plt
+import os
+import gc
+
+RESNET18 = 'RES-NET-18'
+BASIC_AE = 'BASIC_AE'
+AE_RESNET18 = 'AE-RES-NET-18'
+ATTENTION_AE = 'ATTENTION_AE'
+ATTENTION_AE_RESNET18 = 'IMPROVED-AE-RES-NET-18'
+COMBINED_ARCH = [AE_RESNET18, ATTENTION_AE_RESNET18]
+AE_ARCH = [BASIC_AE, ATTENTION_AE]
+CLASSIFIER_ARCH = [RESNET18]
+DATA_PARALLEL = True
+
+PATH_IMG_DIR = r'.\database'
+PATH_FILE_TRAIN = r".\Dataset_files\train_1.txt"
+PATH_FILE_VALIDATION = r".\Dataset_files\val_1.txt"
+PATH_FILE_TEST = r'.\Dataset_files\test_1.txt'
