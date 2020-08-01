c88e1778dd516c8aba7f0b18dc788b75303301d5
omril <olevi@corephotonics.com>
2020-08-01 09:42:30 +0300

    change all classifiers output to be the logits (remove the sigmoid activation).

--- ClassifierModels.py
+++ ClassifierModels.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torchvision
 
+# classifiers based on ResNet
+# all classifiers outputs are the logits, therefore should use Sigmoid to get the prediction probability!
 
 class Resnet18(nn.Module):
 
@@ -10,27 +12,11 @@ class Resnet18(nn.Module):
         self.resnet18 = torchvision.models.resnet18(pretrained=is_trained)
 
         kernelCount = self.resnet18.fc.in_features
-        self.resnet18.fc = nn.Sequential(nn.Linear(kernelCount, num_classes), nn.Sigmoid())
-
-    def forward(self, x):
-        x = self.resnet18(x)
-        return x
-
-
-class Resnet18_V2(nn.Module):
-
-    def __init__(self, num_classes, is_trained):
-        super(Resnet18_V2, self).__init__()
-
-        self.resnet18 = torchvision.models.resnet18(pretrained=is_trained)
-
-        kernelCount = self.resnet18.fc.in_features
         self.resnet18.fc = nn.Sequential(nn.Linear(kernelCount, num_classes))
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x_logits = self.resnet18(x)
-        return x_logits
+        logits = self.resnet18(x)
+        return logits
 
 
 class Resnet34(nn.Module):
@@ -41,11 +27,11 @@ class Resnet34(nn.Module):
         self.resnet34 = torchvision.models.resnet34(pretrained=is_trained)
 
         kernelCount = self.resnet34.fc.in_features
-        self.resnet34.fc = nn.Sequential(nn.Linear(kernelCount, num_classes), nn.Sigmoid())
+        self.resnet34.fc = nn.Sequential(nn.Linear(kernelCount, num_classes))
 
     def forward(self, x):
-        x = self.resnet34(x)
-        return x
+        x_logits = self.resnet34(x)
+        return x_logits
 
 
 class Resnet54(nn.Module):
@@ -56,11 +42,11 @@ class Resnet54(nn.Module):
         self.resnet54 = torchvision.models.resnet54(pretrained=is_trained)
 
         kernelCount = self.resnet54.fc.in_features
-        self.resnet54.fc = nn.Sequential(nn.Linear(kernelCount, num_classes), nn.Sigmoid())
+        self.resnet54.fc = nn.Sequential(nn.Linear(kernelCount, num_classes))
 
     def forward(self, x):
-        x = self.resnet54(x)
-        return x
+        x_logits = self.resnet54(x)
+        return x_logits
 
 
 class DenseNet121(nn.Module):
@@ -70,11 +56,11 @@ class DenseNet121(nn.Module):
 
         self.densenet121 = torchvision.models.densenet121(pretrained=is_trained)
         kernelCount = self.densenet121.classifier.in_features
-        self.densenet121.classifier = nn.Sequential(nn.Linear(kernelCount, num_classes), nn.Sigmoid())
+        self.densenet121.classifier = nn.Sequential(nn.Linear(kernelCount, num_classes))
 
     def forward(self, x):
-        x = self.densenet121(x)
-        return x
+        x_logits = self.densenet121(x)
+        return x_logits
 
 
 class DenseNet169(nn.Module):
@@ -84,11 +70,11 @@ class DenseNet169(nn.Module):
 
         self.densenet169 = torchvision.models.densenet169(pretrained=is_trained)
         kernelCount = self.densenet169.classifier.in_features
-        self.densenet169.classifier = nn.Sequential(nn.Linear(kernelCount, num_classes), nn.Sigmoid())
+        self.densenet169.classifier = nn.Sequential(nn.Linear(kernelCount, num_classes))
         
     def forward (self, x):
-        x = self.densenet169(x)
-        return x
+        x_logits = self.densenet169(x)
+        return x_logits
 
 
 class DenseNet201(nn.Module):
@@ -98,8 +84,8 @@ class DenseNet201(nn.Module):
         
         self.densenet201 = torchvision.models.densenet201(pretrained=is_trained)
         kernelCount = self.densenet201.classifier.in_features
-        self.densenet201.classifier = nn.Sequential(nn.Linear(kernelCount, num_classes), nn.Sigmoid())
+        self.densenet201.classifier = nn.Sequential(nn.Linear(kernelCount, num_classes))
         
     def forward (self, x):
-        x = self.densenet201(x)
-        return x
+        x_logits = self.densenet201(x)
+        return x_logits
